7ffa2c16131e82b18eb88b25e7d9ca84307aa47f
unknown <sebderhy@gmail.com>
2020-03-26 13:55:56 +0200

    try without pkl

--- app/server.py
+++ app/server.py
@@ -10,8 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse, FileResponse
 from starlette.staticfiles import StaticFiles
 import tempfile
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1IaC0PHCnCtsqy2K-B-uYiwIWm0PTtN3C?alt=media&key=AIzaSyBvJeR_YIf7U2wpuDX4Pbw2XntdEU1moxk' 
-export_file_name = 'models/superres-1b.pkl'
+export_file_name = 'models/superres-1b.pth'
 
 path = Path(__file__).parent
 
@@ -19,40 +18,6 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-def my_get_y(x): return path/x.name
-
-def gram_matrix(x):
-    n,c,h,w = x.size()
-    x = x.view(n, c, -1)
-    return (x @ x.transpose(1,2))/(c*h*w)
-
-class FeatureLoss(Module):
-    def __init__(self, m_feat, layer_ids, layer_wgts):
-        self.m_feat = m_feat
-        self.loss_features = [self.m_feat[i] for i in layer_ids]
-        self.hooks = hook_outputs(self.loss_features, detach=False)
-        self.wgts = layer_wgts
-        self.metric_names = ['pixel',] + [f'feat_{i}' for i in range(len(layer_ids))
-              ] + [f'gram_{i}' for i in range(len(layer_ids))]
-
-    def make_features(self, x, clone=False):
-        self.m_feat(x)
-        return [(o.clone() if clone else o) for o in self.hooks.stored]
-    
-    def forward(self, input, target, reduction='mean'):
-        out_feat = self.make_features(target, clone=True)
-        in_feat = self.make_features(input)
-        self.feat_losses = [base_loss(input,target,reduction=reduction)]
-        self.feat_losses += [base_loss(f_in, f_out,reduction=reduction)*w
-                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
-        self.feat_losses += [base_loss(gram_matrix(f_in), gram_matrix(f_out),reduction=reduction)*w**2 * 5e3
-                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
-        if reduction=='none': 
-            self.feat_losses = [f.mean(dim=[1,2,3]) for f in self.feat_losses[:4]] + [f.mean(dim=[1,2]) for f in self.feat_losses[4:]]
-        for n,l in zip(self.metric_names, self.feat_losses): setattr(self, n, l)
-        return sum(self.feat_losses)
-    
-    def __del__(self): self.hooks.remove()
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -64,19 +29,21 @@ async def download_file(url, dest):
 
 
 async def async_setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    # await download_file(export_file_url, path / export_file_name)
     try:
-        learn = torch.load(path/export_file_name, map_location=torch.device('cpu'))
-        print("Model loaded")
-        learn.dls.device = 'cpu'
         dblock = DataBlock(blocks=(ImageBlock, ImageBlock),
                    get_items=get_image_files,
-                   get_y=my_get_y,
-                   splitter=RandomSplitter(),
+                   get_y= lambda x:x,
                    batch_tfms=[Normalize.from_stats(*imagenet_stats)]
                    )
         dbunch_mr = dblock.dataloaders(path, bs=1, val_bs=1, path=path, batch_tfms=[Normalize.from_stats(*imagenet_stats)])         
         dbunch_mr.c = 3        
+
+        learn = unet_learner(dbunch_mr, resnet34, loss_func=F.l1_loss, 
+                     config=unet_config(blur=True, norm_type=NormType.Weight))
+        learn.load(path/export_file_name, map_location=torch.device('cpu'))
+        print("Model loaded")
+        learn.dls.device = 'cpu'
 
         learn.dls = dbunch_mr
         return learn
